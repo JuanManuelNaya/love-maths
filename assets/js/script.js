5f50b1ae7d61b3c36cd1a1c8f9e98d12710825e3
Juan Naya <klanderfil@gmail.com>
2022-04-04 21:13:10 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,13 @@ document.addEventListener("DOMContentLoaded", function () { //when the DOM finis
                 alert("You clicked Submit!");
             }else{
                 let gameType = this.getAttribute("data-type"); //this will tell us what game type we are wanting to run.
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
 
@@ -21,15 +24,20 @@ document.addEventListener("DOMContentLoaded", function () { //when the DOM finis
  * The main game "loop", called when the script is first loaded   
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){ //passing game type as an argument
     
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-}
+    if(gameType === "addition"){ //checking parameter  otherwise throw an error
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}.Aborting!`;
+    }
 
-runGame();
+}
 
 function checkAnswer(){
 
@@ -47,8 +55,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1; //we interrogating our HTML we're going to get the element that has the ID of operand1 , and we set his text content to operand1
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(){
